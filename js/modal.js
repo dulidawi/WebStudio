05be63976e6c3f2a0e1e9c08dfc2a6e88d465322
Oleksandra <157833785+dulidawi@users.noreply.github.com>
2025-03-18 21:43:29 +0100

    123

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,24 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-      document.body.classList.toggle("no-scroll");
-    }
-  })();
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modalBackdrop: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", openModal);
+  refs.closeModalBtn.addEventListener("click", closeModal);
+
+  function openModal() {
+    refs.modalBackdrop.classList.remove("is-hidden");
+    refs.modalBackdrop.classList.add("is-visible");
+    document.body.classList.add("no-scroll");
+  }
+
+  function closeModal() {
+    refs.modalBackdrop.classList.remove("is-visible");
+    document.body.classList.remove("no-scroll");
+    setTimeout(() => {
+      refs.modalBackdrop.classList.add("is-hidden");
+    }, 250); 
+  }
+})();
